6f6b1ce2048683e0be3d6073969d30f00e4e3cc4
Alejandro Guinot <al375927@uji.es>
2022-02-24 12:03:10 +0100

    Integración completa MongoDB (variables de entorno)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule } from "@nestjs/config";
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CardsModule } from './cards/cards.module';
 
 @Module({
-  imports: [CardsModule],
+  imports: [
+    ConfigModule.forRoot(
+      { envFilePath: `access_db.env` }
+    ),
+    CardsModule,
+    // Definir la URI desde MongoDB e introducirla. Esconder nombres y pass con
+    // variables de entorno
+    MongooseModule.forRoot(`mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.3aanw.mongodb.net/${process.env.NAMEDB}?retryWrites=true&w=majority`)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
